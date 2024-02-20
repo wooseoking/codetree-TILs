cca992fcd276784c105d82f97fad6724dd2f3285
Wooseok <64106149+wooseoking@users.noreply.github.com>
2024-02-20 20:27:19 +0900

    Title: 배열 회전, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240220/배열 회전/array-rotation.py
@@ -0,0 +1,51 @@
+n, m, k = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+
+
+def circle(y, x, n, m):
+    if n <= 0 or m <= 0: return
+
+    elements = []
+    y1,x1,y2,x2 = y,x,y + n - 1, x + m - 1
+
+    for j in range(x1,x2):
+        elements.append(a[y1][j])
+
+    for i in range(y1,y2):
+        elements.append(a[i][x2])
+
+    for j in range(x2,x1,-1):
+        elements.append(a[y2][j])
+
+    for i in range(y2,y1,-1):
+        elements.append(a[i][x1])
+
+    rotate = k % len(elements)
+
+    elements = elements[rotate:] + elements[:rotate]
+    idx = 0
+
+    for j in range(x1, x2):
+        a[y1][j] = elements[idx]
+        idx+=1
+
+    for i in range(y1, y2):
+        a[i][x2] = elements[idx]
+        idx += 1
+
+    for j in range(x2, x1, -1):
+        a[y2][j] = elements[idx]
+        idx += 1
+
+    for i in range(y2, y1, -1):
+        a[i][x1] = elements[idx]
+        idx += 1
+
+    circle(y + 1, x + 1, n - 2, m - 2)
+
+
+
+circle(0, 0, n, m)
+
+for rows in a:
+    print(*rows, sep=' ')
