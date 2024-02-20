f7415b0f59576091f5a4f3ddc2f3713c51f98986
Wooseok <64106149+wooseoking@users.noreply.github.com>
2024-02-20 20:55:31 +0900

    Title: 배열 회전, Time: 161ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240220/배열 회전/array-rotation.py
+++ 240220/배열 회전/array-rotation.py
@@ -6,40 +6,32 @@ def circle(y, x, n, m):
     if n <= 0 or m <= 0: return
 
     elements = []
+    indexes = []
+
     y1,x1,y2,x2 = y,x,y + n - 1, x + m - 1
 
     for j in range(x1,x2):
         elements.append(a[y1][j])
+        indexes.append((y1,j))
 
     for i in range(y1,y2):
         elements.append(a[i][x2])
+        indexes.append((i,x2))
 
     for j in range(x2,x1,-1):
         elements.append(a[y2][j])
+        indexes.append((y2,j))
 
     for i in range(y2,y1,-1):
         elements.append(a[i][x1])
-
+        indexes.append((i,x))
 
     elements = elements[1:] + elements[:1]
     idx = 0
 
-    for j in range(x1, x2):
-        a[y1][j] = elements[idx]
-        idx+=1
-
-    for i in range(y1, y2):
-        a[i][x2] = elements[idx]
-        idx += 1
-
-    for j in range(x2, x1, -1):
-        a[y2][j] = elements[idx]
-        idx += 1
-
-    for i in range(y2, y1, -1):
-        a[i][x1] = elements[idx]
-        idx += 1
-
+    for (i,j),element in zip(indexes,elements):
+        a[i][j] = element
+    
     circle(y + 1, x + 1, n - 2, m - 2)
 
 for _ in range(k):
