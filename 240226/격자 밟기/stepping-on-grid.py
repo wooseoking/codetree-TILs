9ac5f5b3b7a5a3d50023303cd32e59730136d83a
Wooseok <64106149+wooseoking@users.noreply.github.com>
2024-02-26 18:39:18 +0900

    Title: 격자 밟기, Time: 835ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240226/격자 밟기/stepping-on-grid.py
@@ -0,0 +1,43 @@
+import math
+
+dy = [-1,1,0,0]
+dx = [0,0,-1,1]
+n = 5
+wall = [[False]*n for _ in range(n)]
+m = int(input())
+for _ in range(m):
+    wy,wx = map(int,input().split())
+    wy-=1
+    wx-=1
+    wall[wy][wx] = True
+
+
+def inside(y,x):
+    return 0<=y<n and 0<=x<n
+
+sry ,srx ,sby,sbx = 0,0,4,4
+visited = [[False]*n for _ in range(n)]
+answer = 0
+def solve(ry,rx,rycnt,by,bx,bycnt):
+    global answer
+
+
+    for rk in range(4):
+        for bk in range(4):
+            nry,nrx = ry + dy[rk] , rx + dx[rk]
+            nby,nbx = by + dy[bk] , bx + dx[bk]
+            if not inside(nry,nrx) or not inside(nby,nbx):continue
+            if visited[nry][nrx] or visited[nby][nbx]:continue
+            if wall[nry][nrx] or wall[nby][nbx]:continue
+            if nry == nby and nrx == nbx and rycnt + bycnt + m == pow(n,2) - 1:
+                answer += 1
+                continue
+            visited[ry][rx] = True
+            visited[by][bx] = True
+            solve(nry,nrx,rycnt + 1,nby,nbx,bycnt + 1)
+            visited[ry][rx] = False
+            visited[by][bx] = False
+
+
+solve(sry,srx,1,sby,sbx,1)
+print(answer)
